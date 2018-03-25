30f6a1f6802fd37b73ab060d335ef5639d7a8a39
rlittletht <rlittle@thetasoft.com>
2018-03-25 13:59:44 -0700

    refactor phone from roster to core

---
+++ RosterCore.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace ArbWeb
+{
+    public class RosterEntryPhones
+    {
+        public string m_sPhone1;
+        public string m_sPhone2;
+        public string m_sPhone3;
+
+        public string CellPhone
+        {
+            get
+            {
+                if (m_sPhone1 != null && m_sPhone1.Contains("C:"))
+                    return m_sPhone1;
+                else if (m_sPhone2 != null && m_sPhone2.Contains("C:"))
+                    return m_sPhone2;
+                else if (m_sPhone3 != null && m_sPhone3.Contains("C:"))
+                    return m_sPhone3;
+                else
+                    return m_sPhone1;
+            }
+        }
+
+        public bool FHasPhoneNumber(int iPhone)
+        {
+            return !String.IsNullOrEmpty(NumberRawForPhone(iPhone));
+        }
+
+        static void ExtractNumberParts(string sNumberRaw, out string sNumber, out string sType)
+        {
+            sNumber = null;
+            sType = null;
+
+            if (String.IsNullOrEmpty(sNumberRaw))
+                {
+                return;
+                }
+
+            if (sNumberRaw.StartsWith("C:"))
+                {
+                sType = "Cellular";
+                }
+            else if (sNumberRaw.StartsWith("W:"))
+                {
+                sType = "Work";
+                }
+            else if (sNumberRaw.StartsWith("H"))
+                {
+                sType = "Home";
+                }
+            else
+                {
+                throw new Exception("Unknown phone type");
+                }
+
+            sNumber = sNumberRaw.Substring(2);
+        }
+
+        static string SConstructPhoneNumberFromParts(string sPhoneNumber, string sTypeDefault)
+        {
+            if (String.IsNullOrEmpty(sPhoneNumber))
+                return sPhoneNumber;
+
+            if (sPhoneNumber.StartsWith("C:") || sPhoneNumber.StartsWith("W:") || sPhoneNumber.StartsWith("H:"))
+                return sPhoneNumber;
+
+            return String.Format("{0}:{1}", sTypeDefault.Substring(0, 1), sPhoneNumber);
+        }
+
+
+        [TestCase("C:425-555-1212", "425-555-1212", "Cellular")]
+        [TestCase("H:425-555-1212", "425-555-1212", "Home")]
+        [TestCase("W:425-555-1212", "425-555-1212", "Work")]
+        [TestCase(null, null, null)]
+        [Test]
+        public static void TestExtractNumberParts(string sNumberRaw, string sNumberExpected, string sTypeExpected)
+        {
+            string sNumberActual, sTypeActual;
+
+            ExtractNumberParts(sNumberRaw, out sNumberActual, out sTypeActual);
+            Assert.AreEqual(sNumberExpected, sNumberActual);
+            Assert.AreEqual(sTypeExpected, sTypeActual);
+        }
+        string NumberRawForPhone(int iPhone)
+        {
+            string sNumberRaw = null;
+
+            switch (iPhone)
+                {
+                case 1:
+                    sNumberRaw = m_sPhone1;
+                    break;
+                case 2:
+                    sNumberRaw = m_sPhone2;
+                    break;
+                case 3:
+                    sNumberRaw = m_sPhone3;
+                    break;
+                }
+
+            return sNumberRaw;
+        }
+        public void GetPhoneNumber(int iPhone, out string sNumber, out string sType)
+        {
+            string sNumberRaw = NumberRawForPhone(iPhone);
+            ExtractNumberParts(sNumberRaw, out sNumber, out sType);
+        }
+
+        public void SetPhoneNumber(int iPhone, string sPhoneNumber, string sTypeDefault)
+        {
+            string sNumberRaw = SConstructPhoneNumberFromParts(sPhoneNumber, sTypeDefault);
+
+            switch (iPhone)
+                {
+                case 1:
+                    m_sPhone1 = sNumberRaw;
+                    break;
+                case 2:
+                    m_sPhone2 = sNumberRaw;
+                    break;
+                case 3:
+                    m_sPhone3 = sNumberRaw;
+                    break;
+                }
+        }
+
+        public void SetNextPhoneNumber(string sPhoneNumber, string sTypeDefault)
+        {
+            int iPhone = 1;
+
+            if (String.IsNullOrEmpty(m_sPhone1))
+                iPhone = 1;
+            else if (String.IsNullOrEmpty(m_sPhone2))
+                iPhone = 2;
+            else if (String.IsNullOrEmpty(m_sPhone3))
+                iPhone = 3;
+            else
+                return;
+
+            SetPhoneNumber(iPhone, sPhoneNumber, sTypeDefault);
+        }
+    }
+}
---
+++ contacts.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace ArbWeb
+{
+    public class ContactEntry
+    {
+        public class TeamRelationship
+        {
+            private string m_sTeamName;
+            private string m_sSport;
+            private string m_sLevel;
+
+            public string TeamName => m_sTeamName;
+            public string Sport => m_sSport;
+            public string Level => m_sLevel;
+
+            public TeamRelationship(string sTeamName, string sSport, string sLevel)
+            {
+                m_sTeamName = sTeamName;
+                m_sSport = sSport;
+                m_sLevel = sLevel;
+            }
+        }
+
+        string m_sEmail;
+        string m_sFirst;
+        string m_sLast;
+        string m_sAddress1;
+        string m_sAddress2;
+        string m_sCity;
+        string m_sState;
+        string m_sZip;
+        string m_sTitle;
+        string m_sLastSignin;
+        string m_sPhone1;
+        string m_sPhone2;
+        string m_sPhone3;
+
+        public string Email { get { return m_sEmail; } set { m_sEmail = value; } } 
+        public string First { get { return m_sFirst; } set { m_sFirst = value; } }
+        public string Last { get { return m_sLast; } set { m_sLast = value; } }
+        public string Address1 { get { return m_sAddress1; } set { m_sAddress1 = value; } }
+        public string Address2 { get { return m_sAddress2; } set { m_sAddress2 = value; } }
+        public string City { get { return m_sCity; } set { m_sCity = value; } }
+        public string State { get { return m_sState; } set { m_sState = value; } }
+        public string Zip { get { return m_sZip; } set { m_sZip = value; } }
+        public string Title { get { return m_sTitle; } set { m_sTitle = value; } }
+        public string LastSignin { get { return m_sLastSignin; } set { m_sLastSignin = value; } }
+        public string Phone1 { get { return m_sPhone1; } set { m_sPhone1 = value; } }
+        public string Phone2 { get { return m_sPhone2; } set { m_sPhone2 = value; } }
+        public string Phone3 { get { return m_sPhone3; } set { m_sPhone3 = value; } }
+
+        #region Phone Numbers
+
+        #endregion
+    }
+}
